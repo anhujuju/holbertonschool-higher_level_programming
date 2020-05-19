30156935a911cb4f0b3aa30bc30732575906a006
anhujuju <anhujuju1@gmail.com>
2020-05-19 20:33:29 +0000

    12

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Square Module - for all your needs, as long as they include printing squares
+"""
+
+
+class Square:
+    """
+    Square class: was created it
+    """
+    def __init__(self, size=0):
+        """
+        instance attribute: size (created it)
+        """
+        self.__size = size
