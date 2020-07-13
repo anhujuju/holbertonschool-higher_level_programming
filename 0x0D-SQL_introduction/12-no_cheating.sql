374282b08ead2fa29c9d667062dc2543705211d7
anhujuju <anhujuju1@gmail.com>
2020-07-13 19:58:53 +0000

    update score bob

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 -- Script lists records of the bable where score >= 10
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
