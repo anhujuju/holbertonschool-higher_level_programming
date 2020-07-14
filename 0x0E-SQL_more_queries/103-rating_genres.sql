e8811e72a4394e1513c3c97de7e42820c3c25408
anhujuju <anhujuju1@gmail.com>
2020-07-14 23:40:58 +0000

    list shows by reating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,3 +1,4 @@
+-- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
 SELECT tv_genres.name AS name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
