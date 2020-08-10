a9e100782ae28eb2415295de7e06287a81ed227f
anhujuju <anhujuju1@gmail.com>
2020-08-10 02:00:08 +0000

    executable

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
-import sys
+"""script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+def get_states():
+    """ Arguments argv to connect to database
+    argv[1]: mysql username
+    argv[2]: mysql password
+    argv[3]: database name
+    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
-    [print(state) for state in cur.fetchall()]
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    # fetchall is necesary for that the print show as a tuple
+    var = cur.fetchall()
+    for i in var:
+        print(i)
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    get_states()
