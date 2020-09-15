c8fac7e7c18042f9c51e78c3a928b56097ff394e
anhujuju <anhujuju1@gmail.com>
2020-09-15 19:15:27 +0000

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 // JS Prints a message depending of arguments
 
 if (process.argv.length === 2) {
-    console.log('No argument');
-  } else if (process.argv.length === 3) {
-    console.log('Argument found');
-  } else {
-    console.log('Arguments found');
-  }
+  console.log('No argument')
+} else if (process.argv.length === 3) {
+  console.log('Argument found')
+} else {
+  console.log('Arguments found')
+}
