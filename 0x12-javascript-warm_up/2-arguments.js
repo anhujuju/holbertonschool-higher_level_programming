6ac4b2726de4d42a169b548cccb8037aa6423fb5
anhujuju <anhujuju1@gmail.com>
2020-09-15 18:52:01 +0000

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// script that prints a message depending of the number of arguments passed
+// JS prints a message depending of the number of arguments passed
 
 if (process.argv[3]) {
     console.log('Arguments found');
